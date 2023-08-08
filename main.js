b32733655f793537ac4f4eaf83b35d3e0a8812bc
Aleyda García <135771286+AleydaGarciaJimenez@users.noreply.github.com>
2023-08-07 18:31:17 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter /*funciona para las palabras que van apareciendo y desapareciendo*/
   .pauseFor(2500)
-  .typeString('<span style="color: #9f4fa3;"> Desarrollo sitios web, me interesa todo sobre cableado estructurado y me gusta todo lo relacionado a la tecnología.</span>')
+  .typeString('<span style="color: #9a7669;"> Desarrollo sitios web, me interesa todo sobre cableado estructurado y me gusta todo lo relacionado a la tecnología.</span>')
   .pauseFor(200)
   .deleteChars(10)
   .start();
